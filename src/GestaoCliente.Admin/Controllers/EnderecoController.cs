b8a4ada4d61686daf848b18d4daa39c3ea3fbea6
Jhonathan Couto <jhony.couto@outlook.com>
2022-02-01 18:05:00 -0300

    EnderecoController foi criada , juntamente com a view: Listar

---
+++ src/GestaoCliente.Admin/Controllers/EnderecoController.cs
@@ -0,0 +1,82 @@
+﻿using GestaoCliente.Domain;
+using GestaoCliente.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoCliente.Admin.Controllers
+{
+    [Route("endereco")]
+    public class EnderecoController : Controller
+    {
+        private readonly IEnderecoService _enderecoService;
+
+        public EnderecoController(IEnderecoService enderecoService)
+        {
+            _enderecoService = enderecoService;
+        }
+
+        [Route("listar")]
+        public IActionResult Listar(int clienteId, string logradouro = null, string numero = null,
+                                    string complemento = null, string cep = null, string estado = null,
+                                    string cidade = null, string bairro = null)
+        {
+            IEnumerable<EnderecoModel> enderecos = _enderecoService.Listar(new Domain.EnderecoModel
+            {
+                ClienteId = clienteId,
+                Logradouro = logradouro,
+                Numero = numero,
+                Complemento = complemento,
+                Cep = cep,
+                Estado = estado,
+                Cidade = cidade,
+                Bairro = bairro
+            });
+            return View(enderecos);
+        }
+
+        [Route("detalhe")]
+        public IActionResult Detalhe(int id)
+        {
+            EnderecoModel endereco = _enderecoService.Obter(new EnderecoModel { Id = id });
+            return View(endereco);
+        }
+
+        [Route("criar")]
+        public IActionResult Criar(int clienteId)
+        {
+            return View(new EnderecoModel { ClienteId = clienteId });
+        }
+        
+        [HttpPost, Route("criar")]
+        public IActionResult Criar(EnderecoModel model)
+        {
+            _enderecoService.Adicionar(model);
+            return RedirectToAction("listar", "cliente");
+        }
+
+        [Route("atualizar")]
+        public IActionResult Atualizar(int id)
+        {
+            EnderecoModel endereco = _enderecoService.Obter(new EnderecoModel { Id = id });
+            return View(endereco);
+        }
+
+        [HttpPost, Route("atualizar")]
+        public IActionResult Atualizar(EnderecoModel model)
+        {
+            _enderecoService.Atualizar(model);
+            EnderecoModel endereco = _enderecoService.Obter(new EnderecoModel { Id = model.Id });
+            return View(endereco);
+        }
+
+        [Route("delete")]
+        public IActionResult Delete(int id)
+        {
+            _enderecoService.Excluir(id);
+            return RedirectToAction("listar", "cliente");
+        }
+    }
+}
