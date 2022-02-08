9983e4af74d3b88eb5c5643538b994c8b10d9fa6
jhonycouto <94021168+jhonycouto@users.noreply.github.com>
2022-02-08 19:17:58 -0300

    Update BaseAPIController.cs

--- src/GestaoCliente.API/Controllers/BaseAPIController.cs
+++ src/GestaoCliente.API/Controllers/BaseAPIController.cs
@@ -11,7 +11,7 @@ namespace GestaoCliente.API.Controllers
     [ApiController]
     public class BaseAPIController : ControllerBase
     {
-    ///
+    ///s
         protected object Sucesso(string mensagem, int codigo, object data = null)
         {
             return Ok(new ResultadoModel
